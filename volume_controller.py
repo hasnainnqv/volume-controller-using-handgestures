7c34f6493e0427841c707b84c95f0a9779525570
syed hasnain ali naqvi <104722638+hasnainnqv@users.noreply.github.com>
2024-03-13 00:59:31 +0500

    Add files via upload

---
+++ volume_controller.py
@@ -0,0 +1,78 @@
+import math
+
+import cv2
+import time
+import numpy as np
+import pycaw
+import handgesture as htm
+
+
+wCam, hcam = 640,480
+
+
+detector = htm.HandDetector(detectionCon=0.7)
+
+cap = cv2.VideoCapture(0)
+cap.set(3,wCam)
+cap.set(4,hcam)
+
+
+####################
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface,POINTER(IAudioEndpointVolume))
+# volume.GetMute()
+# volume.GetMasterVolumeLevel()
+volrange = volume.GetVolumeRange()
+# volume.SetMasterVolumeLevel(-20.0, None)
+maxx ,minn = volrange[1],volrange[0]
+####################
+
+vol= 0
+volBR = 400
+volPER =0
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+
+    lmlist =  detector.findPosition(img,draw = False)
+    if len(lmlist) != 0:
+        x1,y1 = lmlist[4][1],lmlist[4][2]
+        x2, y2 = lmlist[8][1], lmlist[8][2]
+        cx , cy = (x1+x2)//2 , (y1+y2)//2
+
+
+        cv2.circle(img,(x1,y1), 10,(255,0,0),cv2.FILLED)
+        cv2.circle(img,(x2,y2), 10,(255,0,0),cv2.FILLED)
+        cv2.line(img,(x1,y1),(x2,y2),(0,255,0),3)
+        cv2.circle(img, (cx, cy), 10, (255, 0, 0), cv2.FILLED)
+
+
+        length = math.hypot(x2-x1,y2-y1)
+        if length<50:
+            cv2.circle(img, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
+
+        #hand range = 50-300
+        #vol range = -65.25- 0.0
+        vol  = np.interp(length, [50,300],[minn,maxx])
+
+
+        rx1, ry1 = 200, 50
+        rx2, ry2 = 450, 350
+        if (x1>rx1 and  x2<rx2) and   (y1>ry1 and y2< ry2):
+            volume.SetMasterVolumeLevel(vol, None)
+            volBR = np.interp(length, [50, 300], [400, 150])
+            volPER = np.interp(length, [50, 300], [0, 100])
+
+            cv2.rectangle(img, (rx1, ry1), (rx2, ry2), (255, 255, 0), 3)
+
+    cv2.rectangle(img, (50,150),(85,400),(0,255,0),3)
+    cv2.rectangle(img, (50,int(volBR)), (85, 400), (0, 255, 0),cv2.FILLED)
+    cv2.putText(img, f"{str(int(volPER))} %", (18, 78), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 0), 3)
+
+    cv2.imshow('img',img)
+    cv2.waitKey(1)
